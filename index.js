80505b75a7349dbc71dcbff0b1ac9d150e5b5580
Sanketh S G <sankethsg2944@gmail.com>
2025-03-06 23:22:56 +0100

    refactor: update project structure and enhance API for managing persons

--- index.js
+++ index.js
@@ -1,54 +1,96 @@
 
-const http = require('http') //commonJS syntax
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-// import http from 'http' ES6 syntax not supported directly in Node.js
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 
-const app = express()
-app.use(express.json())
-app.use(express.static('dist'))
+const app = express();
+app.use(express.json());
+// app.use(morgan('tiny',{}));
 app.use(cors())
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  // app.get('/', (req,res) => {
-  //   res.send('<h1>Hello World!</h1>')
-  // })
+morgan.token('body', (req) => JSON.stringify(req.body)); //by default body wont be parsed
 
-  app.get('/api/notes', (req,res) => {
-    res.json(notes)
-  })
+app.use(morgan(':method :url :status - :response-time ms :body')); 
+// app.use(morgan)
 
-  app.get('/api/notes/:id', (req,res) => {
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+})
+
+app.get('/info',(req,res) => {
+    const noOfPersons = persons.length;
+    const date =  new Date();
+    res.send(`<p> Phonebook has info for ${noOfPersons} people </p> <p> ${date} </p>`);
+})
+
+app.get('/api/persons/:id', (req,res) => {
     const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    res.json(note)
-  })
+    const person = persons.find(person => person.id === id);
+    if(person){
+        res.json(person);
+    } else {
+        // res.status(404).end();
+        res.sendStatus(404);
+    }
+})
 
-  app.post('/api/notes', (req,res) => {
-    const note = req.body
-    notes = [...notes, note]
-    res.json(note)
-  })
+app.delete('/api/delete/:id', (req,res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+    if(person){
+        persons = persons.filter(person => person.id !== id);
+        res.json(persons);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+app.post('/api/persons', (req,res) => {
+    const body = req.body
+    const id = Math.floor(Math.random() * 1000);
+    if(!body.name || !body.number){
+        return res.status(400).json({
+            error: 'Name or number missing'
+        })
+    } else if(persons.find(person => person.name === body.name)){
+        return res.status(400).json({
+            error: 'Name already exists'
+        })
+    }
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person);
+    res.json(persons);
+})
 
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
